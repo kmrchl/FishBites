7d1a6e2181fd7fd28752446a324faa8f5c97b2eb
Rachel <93565633+kmrchl@users.noreply.github.com>
2024-11-01 02:19:01 +0700

    Menambahkan model dan controller, serta mengatur relasi antar table Produk, Produsen, dan Admin

---
+++ app/Models/Admin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Produsen;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+// use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    protected $table = "admin";
+
+    public function produk()
+    {
+        return $this->hasMany(Produk::class, 'foreign_key', 'local_key');
+    }
+}
