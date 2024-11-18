f706bec608e8f8253b003b7fdd4f12152046064f
yeloranran <rahmawati4487@gmail.com>
2024-11-18 14:34:38 +0700

    company profile

---
+++ resources/views/blog-detail.blade.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fish Bites</title>
+    <meta name="description" content="Iodium | Iodium- minimal onepage personal/portofolio Template. It is built using bootstrap 3.3.2 framework, works totally responsive, easy to customise, well commented codes and seo friendly.">
+    <meta name="keywords" content="minimal, onepage, personal, portfolio, bootstrap">
+    <meta name="author" content="rudhisasmito.com"> 
+	
+	<!-- ==============================================
+	Favicons
+	=============================================== -->
+	<link rel="shortcut icon" href="images/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	
+	<!-- ==============================================
+	CSS
+	=============================================== -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/animate.css" />
+	<link rel="stylesheet" href="css/superslides.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.css" />
+	
+	<!-- ==============================================
+	Google Fonts
+	=============================================== -->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,700,900' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.cdnfonts.com/css/readex-pro" rel="stylesheet">
+	
+	<!-- Custom Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	
+	<!-- COLOR SKINS -->
+	<link rel="stylesheet" type="text/css" href="null" id="color-skins">
+	
+    
+    <script type="text/javascript" src="js/modernizr.min.js"></script>
+	
+    <!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+	
+	<!-- Load page -->
+	<div class="animationload">
+		<div class="loader"></div>
+	</div>
+	
+	<!-- BACK TO TOP SECTION -->
+	<a href="#0" class="cd-top">Top</a>
+	
+<!-- NAVBAR SECTION -->
+<div class="navbar navbar-default main-menu">
+    <div class="aksen-top"></div>
+    <div class="container">
+       <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html"></a>
+      </div>  
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+           <li><a href="index.html" class="">HOME</a></li>
+           <li><a href="index.html#about">ABOUT US</a></li>
+           <li><a href="index.html#product">PRODUCT</a></li>
+           <li class="active"><a href="blog.html">NEWS</a></li>
+           <li><a href="index.html#testimony">TESTIMONIALS</a></li>
+           <li><a href="index.html#contact">CONTACT</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+  
+	
+	<!-- PAGE HEADER SECTION -->
+	<div class="single-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="pull-left">
+						<div class="title">
+							<h1 class="lead wow fadeInDown">Blog</h1>
+							<h2 class="sublead wow fadeInDown" data-wow-delay="500ms">"Judul berita"</h2>
+						</div>
+					</div>
+					<div class="pull-right">
+						<ol class="breadcrumb text-right">
+							<li><a href="index.html">Home</a></li>
+                            <li><a href="blog.html">News</a></li>
+							<li class="active">Blog</li>
+						</ol>
+					</div>					
+				</div>
+			</div>
+		</div>
+        
+    </div>
+	
+	<!-- BLOG SECTION -->
+	<div id="blog" class="section blog" data-slide="6">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					
+					<div class="row">
+						<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+							<div class="blog-item detail wow fadeInDown">
+								
+								<div class="gambar">
+									<div class="date">
+										24<span>Jan 2014</span>
+									</div>
+									<img src="images/blog-2.jpg" alt="" class="img-responsive" />
+								</div>
+								<div class="item-body">
+									<p class="lead">
+									<a href="#" title="">Vel donec et scelerisque vestibulum. Condimentum aliquam, mollit magna velit</a></p>
+									<p>Tempor vestibulum turpis id ligula mi mattis. Eget arcu vitae mauris amet odio. Diam nibh diam, quam elit, libero nostra ut. Pellentesque vehicula. Eget sed, dapibus magna nulla nonummy commodo accumsan morbi, praesent volutpat vel id maecenas</p>
+									<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+									<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+									<br /><br />
+					
+									
+									<div id="comments" class="comment">
+										<h4 class="text-uppercase">Comment <span class="comments">(1)</span></h4>
+										<div class="media comment-block"> <!-- Comment Block #1 -->
+											<div class="media-left media-top">
+												<a href="#">
+												  <img class="media-object" src="images/100px100.jpg" alt="..." width="100" >
+												</a>
+											</div>
+											<div class="media-body">
+												<!-- <a href="#" class="pull-right text-uppercase">Reply</a> -->
+												<h5 class="media-heading">Post by <a href="#">Adinda Rahma</a><small class="">Nov. 05, 2024</small></h5> 
+												<div class="clearfix"></div>
+												Pas dateng ikannya super fresh! Next bakal order lagi
+												<div class="clearfix"></div>
+												
+											</div>
+										</div>
+									</div>
+									<br /><br />
+									<hr />
+									<!-- <div class="comment">
+
+										<h4 class="text-uppercase">Leave a Comment</h4>
+										<form id="contact-form" class="form">
+											<div class="row">
+												<div class="col-md-6">
+													<input type="text" class="form-control" placeholder="Your Name">
+												</div>
+												<div class="col-md-6">
+													<input type="text" class="form-control" placeholder="Your Email">
+												</div>
+											</div>
+											<textarea class="form-control" rows="6" placeholder="Your Comment..."></textarea>
+											<button type="submit" class="btn btn-default btn-primary">Submit Comment</button>
+										</form>
+									</div>	 -->
+								</div>
+
+							</div>
+						</div>
+						
+						
+						<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+							<div class="sidebar">
+								<!-- <div class="widget search"> Search Widget -->
+									<!-- <div class="input-group">
+										<input type="text" class="form-control" placeholder="Search for...">
+										<span class="input-group-btn">
+											<button class="btn btn-default" type="button"><span class="fa fa-search"></span></button>
+										</span>
+									</div>/input-group -->
+								<!-- </div> -->
+								
+								
+								
+								
+								
+								<div class="widget post-tab"> <!-- Posts Tab Widget -->
+									<div role="tabpanel">
+
+
+									</div>
+								</div>
+					
+							</div>
+						</div>
+					</div>
+					
+					
+				</div>
+			</div>
+		</div>
+		
+	</div>
+
+	<!-- FOOTER SECTION -->
+	<div id="footer" class="section footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 wow fadeInDown" data-wow-delay="300ms">
+					<h3>Contact</h3>
+					<p><span>Phone</span>+62 0000 0000</p>
+					<p><span>Fax</span>+62 0000 0000</p>
+					<p><span>Email</span> fishbitesind@gmail.com</p>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 wow fadeInDown" data-wow-delay="600ms">
+					<h3>Address</h3>
+					<p>Jl. Kumbang No.14</p>
+					<p>Babakan, Kec. Bogor Tengah</p>
+					<p>Kota Bogor - Indonesia</p>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 wow fadeInDown" data-wow-delay="900ms">
+					<h3>Follow Us</h3>
+					<div class="sosmed">
+					<p>
+					<a href="#" title=""><span class="fa fa-facebook"></span></a>
+					<a href="#" title=""><span class="fa fa-instagram"></span></a>
+					<a href="#" title=""><span class="fa fa-youtube"></span></a>
+
+					</p>
+					
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 wow fadeInDown" data-wow-delay="1200ms">
+					<h3>Contact</h3>
+					<p>Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi suspendisse.</p>
+				</div>
+				
+			</div>
+		</div>
+		
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<p></p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		
+	</div>	
+
+
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.superslides.js"></script>
+    <script type="text/javascript" src="assets/js/wow.min.js"></script>
+    <script type='text/javascript' src='https://maps.google.com/maps/api/js?sensor=false&#038;ver=4.1.5'></script>
+    <script type="text/javascript" src="assets/js/waypoints.min.js"></script>
+    <script type="text/javascript" src="assets/js/counter.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.counterup.min.js"></script>
+    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.knob.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    
+    <script type="text/javascript" src="assets/js/script.js"></script>
+	<script type="text/javascript" src="assets/js/navigasi.js"></script>
+    
+    
+    <script>
+    jQuery( "#theme-panel .panel-btn" ).click(function(){
+        jQuery( "#theme-panel" ).toggleClass( "panel-close", "panel-open", 1000 );
+        jQuery( "#theme-panel" ).toggleClass( "panel-open", "panel-close", 1000 );
+        return false;
+    });
+    
+    jQuery('.color-switch').click(function(){
+        var title = jQuery(this).attr('title');		
+        var href = 'css/colors/'+title+'.css';
+        
+        //var color =  $(this).css('background-color') ;
+        //$('.dial').attr('data-fgcolor', color);
+        jQuery('#color-skins').attr('href', href);	
+        return false;
+    });	
+    </script>
+    
+    </body>
+    </html>
